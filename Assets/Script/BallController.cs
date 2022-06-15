5b64561c9d424a59e6d1a7266a1e061bb7a84013
reyhantito13 <reyhantito.a@gmail.com>
2022-06-15 10:05:49 +0700

    Adding speed up power up

--- Assets/Script/BallController.cs
+++ Assets/Script/BallController.cs
@@ -6,17 +6,31 @@ public class BallController : MonoBehaviour
 {
     public Vector2 speed;
     public Vector2 resetPosition;
+    public float maxVelocity;
+    public float minVelocity;
     private Rigidbody2D rig;
 
     private void Start()
     {
         rig = GetComponent<Rigidbody2D>();
         rig.velocity = speed;
+        Debug.Log("Ball Speed : " + rig.velocity);
     }
 
     public void ResetBall()
     {
         transform.position = new Vector3(resetPosition.x, resetPosition.y, 2);
         rig.velocity = speed;
+    }
+
+    public void ActivatePUSpeedUp(float magnitude)
+    {
+        Debug.Log("Ball Speed : " + rig.velocity);
+        if (rig.velocity.magnitude > maxVelocity)
+        {
+            rig.velocity = rig.velocity.normalized * maxVelocity;
+        }
+
+        rig.velocity *= magnitude;
     }
 }
